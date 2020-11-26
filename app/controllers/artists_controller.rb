ab2a0c525ed1acbdb2c7458e6c4c1c22fd28a444
Drew Thompson <drewallynthompson@gmail.com>
2020-11-26 09:16:01 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,20 @@
 class ArtistsController < ApplicationController
 
+  def index
+    @artists = Artist.all
+  end 
+  
+  def show 
+    artist_finder
+  end 
+  
+  def new
+    @artist = Artist.new
+  end 
+  
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+  
   private
   
     def artist_finder
