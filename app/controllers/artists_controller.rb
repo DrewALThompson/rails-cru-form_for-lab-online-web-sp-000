f9bcacad5f336a04ab97551fc85f93d4d32e9453
Drew Thompson <drewallynthompson@gmail.com>
2020-11-26 09:11:44 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,12 +2,12 @@ class ArtistsController < ApplicationController
 
   private
   
-  def artist_finder
-    @artist = Artist.find(params[:id])
-  end
+    def artist_finder
+      @artist = Artist.find(params[:id])
+    end
   
-  def artist_params(*args)
-    params.require(:artist).permit(*args)
-  end
+    def artist_params(*args)
+      params.require(:artist).permit(*args)
+    end
 
 end
