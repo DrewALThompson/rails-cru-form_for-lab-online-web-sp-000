41c7e33f78ad9fc3b29e81da4bb73104ea6a1f04
Drew Thompson <drewallynthompson@gmail.com>
2020-11-26 09:19:39 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,32 @@
 class GenresController < ApplicationController
 
+  def index
+    @genres = Genre.all
+  end 
+  
+  def show 
+    genre_finder
+  end 
+  
+  def new
+    @genre = Genre.new
+  end 
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre
+  end 
+  
+  def edit
+    genre_finder
+  end 
+  
+  def update
+    genre_finder
+    @genre.update(genre_params
+  end
+  
   private
   
     def genre_finder
